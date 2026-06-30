2e6e6a2b6e0a3c60b69874ce273c61fbfc04ca2d
rishi <mishrarishi2000@gmail.com>
2026-06-30 18:39:08 +0530

    Pull to Refresh with internet connectivity implemented

--- app/src/main/java/com/rm/postapp/presentation/screen/PostHome/PostContent.kt
+++ app/src/main/java/com/rm/postapp/presentation/screen/PostHome/PostContent.kt
@@ -1,4 +1,4 @@
-package com.rm.postapp.presentation.screen.PostHome
+package com.rm.postapp.presentation.screen.postHome
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
--- app/src/main/java/com/rm/postapp/presentation/screen/PostHome/PostViewModel.kt
+++ app/src/main/java/com/rm/postapp/presentation/screen/PostHome/PostViewModel.kt
@@ -1,9 +1,10 @@
-package com.rm.postapp.presentation.screen.PostHome
+package com.rm.postapp.presentation.screen.postHome
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rm.postapp.domain.models.Post
 import com.rm.postapp.domain.repository.PostRepository
+import com.rm.postapp.domain.utils.NetworkMonitor
 import com.rm.postapp.presentation.utils.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
@@ -13,11 +14,11 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.milliseconds
-import kotlin.time.Duration.Companion.minutes
 
 @HiltViewModel
 class PostViewModel @Inject constructor(
-    private val postRepository: PostRepository
+    private val postRepository: PostRepository,
+    private val networkMonitor: NetworkMonitor
 ) : ViewModel() {
 
     private var _postState = MutableStateFlow(PostUiState())
@@ -26,24 +27,45 @@ class PostViewModel @Inject constructor(
     private var cachePostList = emptyList<Post>()
 
     init {
-        getPosts()
+        getPosts(false)
     }
 
-    private fun getPosts() {
-        viewModelScope.launch {
+    fun getPosts(isRefresh: Boolean) {
+        if (isRefresh) {
+            _postState.update { it.copy(isRefreshing = true) }
+        } else {
             _postState.update { it.copy(postState = UiState.Loading) }
+        }
 
-            delay(2000.milliseconds)
+        if (networkMonitor.isConnected()) {
+            viewModelScope.launch {
 
-            try {
-                val postList = postRepository.getPosts()
-                cachePostList = postList
-                _postState.update { it.copy(postState = UiState.Success(postList)) }
+                delay(2000.milliseconds)
 
-            } catch (e: Exception) {
-                _postState.update { it.copy(postState = UiState.Error(e.toString())) }
+                try {
+                    val postList = postRepository.getPosts()
+                    cachePostList = postList
+                    _postState.update { it.copy(postState = UiState.Success(postList), isRefreshing = false) }
+
+                } catch (e: Exception) {
+                    if (isRefresh) {
+                        _postState.update { it.copy(errorMessage = e.toString(), isRefreshing = false) }
+                    } else {
+                        _postState.update { it.copy(postState = UiState.Error(e.toString()), errorMessage = e.toString(), isRefreshing = false) }
+                    }
+                }
+            }
+        } else {
+            if (isRefresh) {
+                _postState.update { it.copy(errorMessage = "No Internet", isRefreshing = false) }
+            } else {
+                _postState.update { it.copy(postState = UiState.Error("No Internet"), errorMessage = "No Internet", isRefreshing = false) }
             }
         }
+    }
+
+    fun dismissError() {
+        _postState.update { it.copy(errorMessage = null) }
     }
 
     fun onSearchTextQueryChange(searchQuery: String) {
