f219633d32151a1f7d9fca363947dbedfbbba989
abhishekprakash <prakashabhishek010@gmail.com>
2026-07-19 00:54:36 +0530

    - handled review comment

--- app/src/main/java/com/rm/postapp/data/repository/PostShareManagerImpl.kt
+++ app/src/main/java/com/rm/postapp/data/repository/PostShareManagerImpl.kt
@@ -1,7 +1,9 @@
 package com.rm.postapp.data.repository
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.rm.postapp.domain.models.Post
 import com.rm.postapp.domain.repository.PostShareManager
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -9,13 +11,12 @@ import javax.inject.Inject
 
 class PostShareManagerImpl @Inject constructor(
     @param:ApplicationContext private val context: Context
-): PostShareManager {
+) : PostShareManager {
     override fun sharePost(post: Post) {
-        val shareText = "Check out this post!\n\nTitle:: ${post.title}\n\n${post.body}"
 
         val shareIntent = Intent(Intent.ACTION_SEND).apply {
             type = "text/plain"
-            putExtra(Intent.EXTRA_TEXT, shareText)
+            putExtra(Intent.EXTRA_TEXT, createShareContent(post))
         }
 
         val chooserIntent = Intent.createChooser(
@@ -25,6 +26,22 @@ class PostShareManagerImpl @Inject constructor(
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
 
-        context.startActivity(chooserIntent)
+        try {
+            context.startActivity(chooserIntent)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("Share", "No app found to share", e)
+        }
+    }
+
+    fun createShareContent(post: Post): String {
+        return buildString {
+            appendLine("📄 Check out this post!")
+            appendLine()
+            appendLine("Title:")
+            appendLine(post.title)
+            appendLine()
+            append(post.body)
+
+        }
     }
 }
